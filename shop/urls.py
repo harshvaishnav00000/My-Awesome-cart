cf57d2b71f643f3f94f92e8b44b80d002832c77c
harshvaishnav00000 <harshvaishnav00000@gmail.com>
2020-08-02 20:13:42 +0530

    first commit

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="shophome"),
+    path('about/', views.about, name="aboutus"),
+    path('contact/', views.contact, name="contactus"),
+    path('tracker/', views.tracker, name="trackingstatus"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('products/<int:myid>', views.productView, name="productView"),
+    path('search/', views.search, name="search"),
+]
